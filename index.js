aeb9bce9f1f46da90f4769a4eb9ee3bd5897211d
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2024-04-17 14:51:26 +0000

    feat: adiciona endpoint de criação de item

--- index.js
+++ index.js
@@ -17,4 +17,25 @@ app.get('/ooi', function (req, res) {
     res.send(lista)
   })
 
+  // Endpoint Read By id -> [GET] /item/:id
+  app.get('/item/:id', function (req, res) {
+    const id = req.params.id
+    const item = lista[id-1]
+
+    res.send(`Item: ${item}`)
+  })
+
+  // Especificamos que o corpo das requisições será em JSON na nossa aplicação
+  app.use(express.json())
+
+  // Endpoint Create -> [POST] /item
+  app.post('/item', function(req, res) {
+    // Pegamos o item através do corpo da requisição
+    // no obj JSON, pegamos a propriedade nome
+    const item = req.body.nome
+
+    lista.push(item)
+    res.send(`Item adicionado com sucesso: ${item}`)
+  })
+
 app.listen(3000)
