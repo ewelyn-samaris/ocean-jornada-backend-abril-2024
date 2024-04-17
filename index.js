979fc9d359c5c9c4e06593cffc6a4118116675bf
Ewelyn Sâmaris <ewelynsamaris@hotmail.com>
2024-04-17 13:27:42 +0000

    feat: adiciona endpoint /items

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/oi', function (req, res) {
-    res.send('Olá ewelyn!')
+app.get('/ooi', function (req, res) {
+    res.send('Olá Ewelyn!')
+  })
+
+  // lista de items
+  const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+  // endpoint Read All -> [GET] /items
+  app.get('/items', function (req, res){
+    res.send(lista)
   })
 
 app.listen(3000)
